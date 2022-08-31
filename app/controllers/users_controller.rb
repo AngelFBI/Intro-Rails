5577dd80d475521703e6c6cf452257ffb2566df1
AngelFBI <angel.bejarano@enroutesystems.com>
2022-08-31 17:48:21 -0500

    Added destroy method
    - Bug and link to can't delete anything

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show,:edit,:update]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
   before_action :require_user, only: [:edit, :update]
-  before_action :require_same_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def show
     @articles = @user.articles.paginate(page: params[:page], per_page: 3)
@@ -27,10 +27,17 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    @user.destroy
+    session[:user_id] = nil
+    flash[:notice] = "Account and all associated articles successfully deleted"
+    redirect_to root_path
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
-      session[:user_id] = user.id
+      session[:user_id] = @user.id
       flash[:notice] = "Welcome to my project, you have successfully sign up"
       redirect_to articles_path
     else
