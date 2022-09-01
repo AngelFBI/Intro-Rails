8f73e60be5f120f97b2fad611b8f19b1799a97d6
AngelFBI <angel.bejarano@enroutesystems.com>
2022-09-01 17:00:51 -0500

    Created set_category for categories controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,6 @@
 class CategoriesController < ApplicationController
   before_action :require_admin, except: [:index, :show ]
+  before_action :set_category, only: [:show, :edit, :update]
 
   def new
     @category = Category.new
@@ -10,7 +11,6 @@ class CategoriesController < ApplicationController
   end
 
   def show
-    @category = Category.find(params[:id])
     @articles = @category.articles.paginate(page: params[:page], per_page: 3)
   end
 
@@ -25,11 +25,9 @@ class CategoriesController < ApplicationController
   end
 
   def edit
-    @category = Category.find(params[:id])
   end
 
   def update
-    @category = Category.find(params[:id])
     if @category.update(category_params)
       flash[:notice] = 'Category name updated successfully'
       redirect_to @category
@@ -41,6 +39,10 @@ class CategoriesController < ApplicationController
   private
   def category_params
     params.require(:category).permit(:name)
+  end
+
+  def set_category
+    @category = Category.find(params[:id])
   end
 
   def require_admin
